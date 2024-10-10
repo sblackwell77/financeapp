23720c3de68f641458e8e525ff87227de9a4020a
sblackwell77 <167127357+sblackwell77@users.noreply.github.com>
2024-10-09 23:51:55 -0700

    urls.py

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.transaction_list, name='transaction_list'),  # List transactions
-    path('add/', views.add_transaction, name='add_transaction'),  # Add transaction
-    path('stocks/', views.stock_prices, name='stock_prices'),  # Stock prices
+    path('', views.transaction_list, name='transaction_list'), 
+    path('add/', views.add_transaction, name='add_transaction'), 
+    path('stocks/', views.stock_prices, name='stock_prices'),  
 ]
 
