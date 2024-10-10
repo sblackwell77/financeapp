e212ce24bd0217d4864980ee0f8c224233498962
sblackwell77 <167127357+sblackwell77@users.noreply.github.com>
2024-10-09 23:55:28 -0700

    urls.py

--- finance_dashboard/urls.py
+++ finance_dashboard/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('transactions.urls')),  # Include your transactions app's URLs
+    path('', include('transactions.urls')),  
 ]
 
 
